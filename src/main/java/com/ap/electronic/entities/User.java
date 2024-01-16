371dd1601e6bf12728315693d501ecf5954fb050
akshaypatil13 <akshaypatil13@10.61.100.216>
2024-01-16 23:16:53 +0530

    Revert "Revert "Revert "lombok changes removed"""
    This reverts commit 5c5ebf1b21581b1939ca4286bf1db9cbc08a6e2c.

--- src/main/java/com/ap/electronic/entities/User.java
+++ src/main/java/com/ap/electronic/entities/User.java
@@ -5,8 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 	@Id
@@ -19,87 +26,5 @@ public class User {
 	private String gender;
 	private String about;
 	private String imageName;
-	
-	public User() {
-		//
-	}
-
-	public User(String userId, String username, String email, String password, String gender, String about,
-			String imageName) {
-		super();
-		this.userId = userId;
-		this.username = username;
-		this.email = email;
-		this.password = password;
-		this.gender = gender;
-		this.about = about;
-		this.imageName = imageName;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getAbout() {
-		return about;
-	}
-
-	public void setAbout(String about) {
-		this.about = about;
-	}
-
-	public String getImageName() {
-		return imageName;
-	}
-
-	public void setImageName(String imageName) {
-		this.imageName = imageName;
-	}
-
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", username=" + username + ", email=" + email + ", password=" + password
-				+ ", gender=" + gender + ", about=" + about + ", imageName=" + imageName + "]";
-	}
-	
-	
-	
-	
 
 }
